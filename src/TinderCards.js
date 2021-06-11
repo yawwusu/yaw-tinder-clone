f04c0388f66ffcaaec2272da5ee354a34a62c952
yawwusu <yawwusugh@gmail.com>
2021-06-11 13:50:43 +0200

    Connected app to firebase firestore db

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,35 +1,37 @@
 import React from "react";
 import TinderCard from "react-tinder-card";
+import { db } from "./firebase";
 import "./TinderCards.css";
 
 function TinderCards() {
-  const [people, setPeople] = React.useState([
-    {
-      name: "steve jobs",
-      url: "https://www.ilmattino.it/photos/HIGH/12/80/1231280_50907_c4_steve_jobsc.jpg",
-    },
-    {
-      name: "yaw",
-      url: "https://i1.rgstatic.net/ii/profile.image/441069365141505-1482170539676_Q512/Yaw-Owusu-Agyemang.jpg",
-    },
-  ]);
+  const [people, setPeople] = React.useState([]);
+  console.log("people", people);
+
+  React.useEffect(() => {
+    const unsubscribe = db
+      .collection("people")
+      .onSnapshot((snapshot) =>
+        setPeople(
+          snapshot.docs.map((doc) => ({ id: doc.id, info: doc.data() }))
+        )
+      );
+    return () => unsubscribe();
+  }, []);
 
   return (
     <div>
-      <h1>TinderCards</h1>
-
       <div className="Card-container">
         {people.map((person) => (
           <TinderCard
-            key={person.name}
+            key={person.id}
             className="swipe"
             preventSwipe={["up", "down"]}
           >
             <div
-              style={{ backgroundImage: `url(${person.url})` }}
+              style={{ backgroundImage: `url(${person.info.url})` }}
               className="Card"
             >
-              <h3>{person.name}</h3>
+              <h3>{person.info.name}</h3>
             </div>
           </TinderCard>
         ))}
